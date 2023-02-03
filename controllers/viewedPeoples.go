e63a2b9e9de8dcb7bcb440a89bbf174fb78c3755
UP <akarealup@gmail.com>
2023-02-03 16:21:08 +0800

    修复ip数据查询的bug

--- controllers/viewedPeoples.go
+++ controllers/viewedPeoples.go
@@ -1,21 +1,22 @@
 package controllers
 
 import (
+	"errors"
 	"goblog/database"
 	"goblog/models"
-	"log"
 
 	// "log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func HandleViewedPeoples(c *gin.Context) {
 	remoteip := c.ClientIP()
-	// if remoteip == "::1" {
-	// 	remoteip = "127.0.0.1"
-	// }
+	if remoteip == "::1" {
+		remoteip = "127.0.0.1"
+	}
 
 	// 尝试根据ip创建记录
 	err := CreateViewLog(remoteip)
@@ -34,17 +35,17 @@ func HandleViewedPeoples(c *gin.Context) {
 // 增加一条访问记录
 func CreateViewLog(ip string) error {
 	err := FindViewedLogByIP(ip)
-	if err != nil { // 如果没找记录
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) { // 如果没找到记录
+		vplog := models.ViwedPeoples{
+			IPAddress: ip,
+		}
+
+		err = database.DB.Create(&vplog).Error
+		if err != nil {
+			return err
+		}
 		return err
-	}
-
-	// 如果没找到记录
-	vplog := models.ViwedPeoples{
-		IPAddress: ip,
-	}
-
-	err = database.DB.Create(&vplog).Error
-	if err != nil {
+	} else if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) { // 其他报错
 		return err
 	}
 
@@ -54,11 +55,11 @@ func CreateViewLog(ip string) error {
 // 根据ip查询记录
 func FindViewedLogByIP(ip string) error {
 	// log.Println(ip)
-	vplog := models.ViwedPeoples{}
-	result := database.DB.Find(&vplog, "ip_address = ?", ip)
+	vplog := models.ViwedPeoples{IPAddress: ip}
+	result := database.DB.First(&vplog)
 
 	if result.Error != nil {
-		log.Println(result.Error)
+		// log.Println(result.Error)
 		return result.Error
 	}
 
