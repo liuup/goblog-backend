85d52b9e7673df081d5ea69fd2793179a55e4528
UP <akarealup@gmail.com>
2023-02-06 16:25:54 +0800

    add: 增加定时负载监控查看api

--- controllers/serverStatusCron.go
+++ controllers/serverStatusCron.go
@@ -1,14 +1,26 @@
 package controllers
 
 import (
+	"goblog/database"
+	"goblog/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// TODO: 使用cron定时任务来获取服务器负载信息
+// 使用cron定时任务来获取服务器负载信息
 func HandleServerStatusCron(c *gin.Context) {
+	ss := models.ServerStatus{}
+
+	result := database.DB.Last(&ss)
+	if result.Error != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"server_status_cron": "server cron ok",
+		"data": ss,
 	})
 }
